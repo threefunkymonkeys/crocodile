f0c13197138a37854c91bc3fb00392c871159c58
Leonardo Mateo <leonardomateo@gmail.com>
2014-07-16 21:32:00 -0300

    Moves CrocodileProcess class to its own file

---
+++ lib/crocodile/crocodile_process.rb
@@ -0,0 +1,51 @@
+class CrocodileProcess
+  def initialize(job_name, pids_dir)
+    @name = job_name
+    @job_path = File.expand_path("jobs/#{job_name}")
+    @pid_path = File.expand_path("#{job_name}.pid", pids_dir)
+  end
+
+  def start
+    require @job_path
+    job_class = constantize(@name)
+
+    Signal.trap("INT")  { EventMachine.stop }
+    Signal.trap("TERM") { EventMachine.stop }
+
+    EventMachine.run do
+      timer = EventMachine::PeriodicTimer.new(job_class.interval || 60) do
+        job_class.logger.info job_class.message
+        job_class.run
+        if job_class.one_run_only
+          timer.cancel
+          EventMachine.stop
+        end
+      end
+    end
+  end
+
+  def stop
+    if File.exists?(@pid_path)
+      Process.kill :TERM, File.open(@pid_path).read.strip.to_i
+      File.delete(@pid_path)
+      true
+    else
+      false
+    end
+  end
+
+  def daemonize!
+    Process.daemon(true)
+    File.open(@pid_path, File::RDWR|File::EXCL|File::CREAT, 0600) { |io| io.write(Process.pid) }
+  end
+
+  def clean
+    if File.exists?(@pid_path)
+      File.delete(@pid_path)
+    end
+  end
+
+  def constantize(name)
+    Kernel.const_get(name.split("_").push("job").map(&:capitalize).join)
+  end
+end
